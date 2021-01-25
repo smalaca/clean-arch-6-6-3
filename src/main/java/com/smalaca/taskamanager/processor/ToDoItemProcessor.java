1e19e95a3e95e2ee935917b9ccb88590f5fda36b
smalaca <ab13.krakow@gmail.com>
2021-01-25 11:51:25 +0100

    remove integration with travis and sonar

--- src/main/java/com/smalaca/taskamanager/processor/ToDoItemProcessor.java
+++ src/main/java/com/smalaca/taskamanager/processor/ToDoItemProcessor.java
@@ -15,27 +15,18 @@ import com.smalaca.taskamanager.service.CommunicationService;
 import com.smalaca.taskamanager.service.ProjectBacklogService;
 import com.smalaca.taskamanager.service.SprintBacklogService;
 import com.smalaca.taskamanager.service.StoryService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import static com.smalaca.taskamanager.model.enums.ToDoItemStatus.DONE;
 
 @Component
 public class ToDoItemProcessor {
-    private final StoryService storyService;
-    private final EventsRegistry eventsRegistry;
-    private final ProjectBacklogService projectBacklogService;
-    private final CommunicationService communicationService;
-    private final SprintBacklogService sprintBacklogService;
-
-    public ToDoItemProcessor(
-            StoryService storyService, EventsRegistry eventsRegistry, ProjectBacklogService projectBacklogService,
-            CommunicationService communicationService, SprintBacklogService sprintBacklogService) {
-        this.storyService = storyService;
-        this.eventsRegistry = eventsRegistry;
-        this.projectBacklogService = projectBacklogService;
-        this.communicationService = communicationService;
-        this.sprintBacklogService = sprintBacklogService;
-    }
+    @Autowired private StoryService storyService;
+    @Autowired private EventsRegistry eventsRegistry;
+    @Autowired private ProjectBacklogService projectBacklogService;
+    @Autowired private CommunicationService communicationService;
+    @Autowired private SprintBacklogService sprintBacklogService;
 
     public void processFor(ToDoItem toDoItem) {
         switch (toDoItem.getStatus()) {
