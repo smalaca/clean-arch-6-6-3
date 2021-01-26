920641517d673bb52b9eed3db7306db52f1a02b1
smalaca <ab13.krakow@gmail.com>
2021-01-26 19:37:43 +0100

    cover processing of ToDoItem defined with tests

--- src/test/java/com/smalaca/taskamanager/processor/ToDoItemProcessorTest.java
+++ src/test/java/com/smalaca/taskamanager/processor/ToDoItemProcessorTest.java
@@ -1,10 +1,15 @@
 package com.smalaca.taskamanager.processor;
 
+import com.smalaca.taskamanager.events.EpicReadyToPrioritize;
 import com.smalaca.taskamanager.events.StoryApprovedEvent;
 import com.smalaca.taskamanager.events.StoryDoneEvent;
 import com.smalaca.taskamanager.events.TaskApprovedEvent;
 import com.smalaca.taskamanager.events.ToDoItemReleasedEvent;
+import com.smalaca.taskamanager.exception.UnsupportedToDoItemType;
 import com.smalaca.taskamanager.model.entities.Epic;
+import com.smalaca.taskamanager.model.entities.ProductOwner;
+import com.smalaca.taskamanager.model.entities.Project;
+import com.smalaca.taskamanager.model.entities.Sprint;
 import com.smalaca.taskamanager.model.entities.Story;
 import com.smalaca.taskamanager.model.entities.Task;
 import com.smalaca.taskamanager.model.enums.ToDoItemStatus;
@@ -18,11 +23,15 @@ import org.junit.jupiter.api.Test;
 import org.mockito.ArgumentCaptor;
 
 import static com.smalaca.taskamanager.model.enums.ToDoItemStatus.APPROVED;
+import static com.smalaca.taskamanager.model.enums.ToDoItemStatus.DEFINED;
 import static com.smalaca.taskamanager.model.enums.ToDoItemStatus.DONE;
 import static com.smalaca.taskamanager.model.enums.ToDoItemStatus.IN_PROGRESS;
 import static com.smalaca.taskamanager.model.enums.ToDoItemStatus.RELEASED;
 import static com.smalaca.taskamanager.model.enums.ToDoItemStatus.TO_BE_DEFINED;
+import static java.util.Arrays.asList;
+import static java.util.Collections.emptyList;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.BDDMockito.then;
 import static org.mockito.Mockito.mock;
@@ -206,5 +215,95 @@ class ToDoItemProcessorTest {
         then(storyService).should().updateProgressOf(story, task);
         verifyNoMoreInteractions(storyService);
         verifyNoInteractions(eventsRegistry, projectBacklogService, communicationService, sprintBacklogService);
+    }
+
+    @Test
+    void shouldProcessDefinedEpic() {
+        ArgumentCaptor<EpicReadyToPrioritize> captor = ArgumentCaptor.forClass(EpicReadyToPrioritize.class);
+        Epic epic = mock(Epic.class);
+        given(epic.getStatus()).willReturn(DEFINED);
+        given(epic.getId()).willReturn(TO_DO_ITEM_ID);
+        Project project = mock(Project.class);
+        ProductOwner productOwner = mock(ProductOwner.class);
+        given(project.getProductOwner()).willReturn(productOwner);
+        given(epic.getProject()).willReturn(project);
+
+        processor.processFor(epic);
+
+        then(projectBacklogService).should().putOnTop(epic);
+        then(eventsRegistry).should().publish(captor.capture());
+        assertThat(captor.getValue().getEpicId()).isEqualTo(TO_DO_ITEM_ID);
+        then(communicationService).should().notify(epic, productOwner);
+        verifyNoMoreInteractions(projectBacklogService, eventsRegistry, communicationService);
+        verifyNoInteractions(storyService, sprintBacklogService);
+    }
+
+    @Test
+    void shouldProcessDefinedStoryWithNoTasks() {
+        Story story = mock(Story.class);
+        given(story.getStatus()).willReturn(DEFINED);
+        given(story.getTasks()).willReturn(emptyList());
+        Project project = mock(Project.class);
+        given(story.getProject()).willReturn(project);
+
+        processor.processFor(story);
+
+        then(projectBacklogService).should().moveToReadyForDevelopment(story, project);
+        verifyNoMoreInteractions(projectBacklogService);
+        verifyNoInteractions(eventsRegistry, storyService, communicationService, sprintBacklogService);
+    }
+
+    @Test
+    void shouldProcessDefinedAndNotAssignedStoryWithTasks() {
+        Story story = mock(Story.class);
+        given(story.getStatus()).willReturn(DEFINED);
+        Task task = mock(Task.class);
+        given(story.getTasks()).willReturn(asList(task));
+        given(story.isAssigned()).willReturn(false);
+        Project project = mock(Project.class);
+        given(story.getProject()).willReturn(project);
+
+        processor.processFor(story);
+
+        then(communicationService).should().notifyTeamsAbout(story, project);
+        verifyNoMoreInteractions(communicationService);
+        verifyNoInteractions(eventsRegistry, storyService, projectBacklogService, sprintBacklogService);
+    }
+
+    @Test
+    void shouldProcessDefinedAndAssignedStoryWithTasks() {
+        Story story = mock(Story.class);
+        given(story.getStatus()).willReturn(DEFINED);
+        Task task = mock(Task.class);
+        given(story.getTasks()).willReturn(asList(task));
+        given(story.isAssigned()).willReturn(true);
+
+        processor.processFor(story);
+
+        verifyNoInteractions(eventsRegistry, storyService, projectBacklogService, communicationService, sprintBacklogService);
+    }
+
+    @Test
+    void shouldProcessDefinedTask() {
+        Task task = mock(Task.class);
+        given(task.getStatus()).willReturn(DEFINED);
+        Sprint currentSprint = mock(Sprint.class);
+        given(task.getCurrentSprint()).willReturn(currentSprint);
+
+        processor.processFor(task);
+
+        then(sprintBacklogService).should().moveToReadyForDevelopment(task, currentSprint);
+        verifyNoMoreInteractions(sprintBacklogService);
+        verifyNoInteractions(eventsRegistry, storyService, projectBacklogService, communicationService);
+    }
+
+    @Test
+    void shouldProcessDefinedToDoItem() {
+        ToDoItem toDoItem = mock(ToDoItem.class);
+        given(toDoItem.getStatus()).willReturn(DEFINED);
+
+        assertThrows(UnsupportedToDoItemType.class, () -> processor.processFor(toDoItem));
+
+        verifyNoInteractions(eventsRegistry, storyService, projectBacklogService, communicationService, sprintBacklogService);
     }
 }
