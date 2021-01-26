b81ecab197e57c3dc7e7ab10777903e8f4076304
smalaca <ab13.krakow@gmail.com>
2021-01-26 19:41:12 +0100

    move processing of ToDoItem defined to ToDoItemDefinedState

--- src/main/java/com/smalaca/taskamanager/processor/ToDoItemProcessor.java
+++ src/main/java/com/smalaca/taskamanager/processor/ToDoItemProcessor.java
@@ -1,10 +1,5 @@
 package com.smalaca.taskamanager.processor;
 
-import com.smalaca.taskamanager.events.EpicReadyToPrioritize;
-import com.smalaca.taskamanager.exception.UnsupportedToDoItemType;
-import com.smalaca.taskamanager.model.entities.Epic;
-import com.smalaca.taskamanager.model.entities.Story;
-import com.smalaca.taskamanager.model.entities.Task;
 import com.smalaca.taskamanager.model.enums.ToDoItemStatus;
 import com.smalaca.taskamanager.model.interfaces.ToDoItem;
 import com.smalaca.taskamanager.registry.EventsRegistry;
@@ -44,45 +39,12 @@ public class ToDoItemProcessor {
     public void processFor(ToDoItem toDoItem) {
         initToDoItemStates();
 
-        if (toDoItem.getStatus() == ToDoItemStatus.DEFINED) {
-            processDefined(toDoItem);
-        } else {
-            states.get(toDoItem.getStatus()).process(toDoItem);
-        }
+        states.get(toDoItem.getStatus()).process(toDoItem);
     }
 
     private void initToDoItemStates() {
         if (states == null) {
-            states = new ToDoItemStatesFactory().getAll(storyService, eventsRegistry);
-        }
-    }
-
-    private void processDefined(ToDoItem toDoItem) {
-        if (toDoItem instanceof Story) {
-            Story story = (Story) toDoItem;
-            if (story.getTasks().isEmpty()) {
-                projectBacklogService.moveToReadyForDevelopment(story, story.getProject());
-            } else {
-                if (!story.isAssigned()) {
-                    communicationService.notifyTeamsAbout(story, story.getProject());
-                }
-            }
-        } else {
-            if (toDoItem instanceof Task) {
-                Task task = (Task) toDoItem;
-                sprintBacklogService.moveToReadyForDevelopment(task, task.getCurrentSprint());
-            } else {
-                if (toDoItem instanceof Epic) {
-                    Epic epic = (Epic) toDoItem;
-                    projectBacklogService.putOnTop(epic);
-                    EpicReadyToPrioritize event = new EpicReadyToPrioritize();
-                    event.setEpicId(epic.getId());
-                    eventsRegistry.publish(event);
-                    communicationService.notify(toDoItem, toDoItem.getProject().getProductOwner());
-                } else {
-                    throw new UnsupportedToDoItemType();
-                }
-            }
+            states = new ToDoItemStatesFactory().getAll(storyService, eventsRegistry, projectBacklogService, communicationService, sprintBacklogService);
         }
     }
 }
